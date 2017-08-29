8dcf6b1a6e84c7da519596ec854132f403989ed8
Vlad <vlad@v-lad.org>
2017-08-29 00:32:40 -0700

    Remove bad jerb

--- jobs/example9Jobs.groovy
+++ jobs/example9Jobs.groovy
@@ -6,47 +6,9 @@ folder(basePath) {
 }
 
 job("$basePath/grails-legacy-build") {
-    scm {
-        cvsscm {
-            repositories {
-                cvsRepository {
-                    cvsRoot repo
-                    passwordRequired false
-                    password null
-                    compressionLevel(-1)
-                    repositoryBrowser {}
-                    repositoryItems {
-                        cvsRepositoryItem {
-                            modules {
-                                cvsModule {
-                                    remoteName 'sheehan/grails-example'
-                                    localName '.'
-                                    projectsetFileName null
-                                }
-                            }
-                            location {
-                                headRepositoryLocation()
-                            }
-                        }
-                    }
-                }
-            }
-            canUseUpdate true
-            pruneEmptyDirectories true
-            legacy false
-            skipChangeLog false
-            disableCvsQuiet false
-            cleanOnFailedUpdate false
-            forceCleanCopy false
-        }
-    }
     triggers {
         scm('H/5 * * * *')
     }
     steps {
-        grails {
-            useWrapper true
-            targets(['test-app', 'war'])
-        }
     }
 }
